0fc51e40470d12a2bafdc25fb4fa26a231282d14
Nils VAN ZUIJLEN <nils.van-zuijlen@mailo.com>
2021-03-01 09:43:17 +0100

    Make installation work
    - Use vendored source
    - Update LDAP settings
    - Update nginx config
    - Rename _common(s).sh
    - Use a CSRF aware local_curl
    - Remove/update old patches
    
    Also trim remove, restore, upgrade and backup scripts

--- conf/ldap.sql
+++ conf/ldap.sql
@@ -1,18 +1,28 @@
-REPLACE INTO setting (`name`, `value`, `module_id`) VALUES
-('auth.ldap.enabled', '1', 'user'), 
-('auth.ldap.hostname', 'localhost', 'user'),
-('auth.ldap.port', '389', 'user'),
-('auth.ldap.encryption', '', 'user'), 
-('auth.ldap.username', '', 'user'), 
-('auth.ldap.password', '', 'user'), 
-('auth.ldap.baseDn', 'ou=users,dc=yunohost,dc=org', 'user'), 
-('auth.ldap.loginFilter', '(uid=%s)', 'user'),
-('auth.ldap.userFilter', 'objectClass=mailAccount', 'user'),
-('auth.ldap.emailAttribute', 'mail', 'user'), 
-('auth.ldap.usernameAttribute', 'uid', 'user'),
-('auth.ldap.refreshUsers', '1', 'user'),
+BEGIN;
+
+DELETE FROM setting WHERE
+(`name` = 'auth.anonymousRegistration') OR
+(`name` = 'auth.allowGuestAccess') OR
+(`name` = 'auth.internalUsersCanInvite');
+
+INSERT INTO setting (`name`, `value`, `module_id`) VALUES
+('enabled', '1', 'ldap'), 
+('hostname', 'localhost', 'ldap'),
+('port', '389', 'ldap'),
+('encryption', '', 'ldap'), 
+('username', '', 'ldap'), 
+('password', '', 'ldap'), 
+('baseDn', 'ou=users,dc=yunohost,dc=org', 'ldap'), 
+('loginFilter', '(uid=%s)', 'ldap'),
+('userFilter', 'objectClass=mailAccount', 'ldap'),
+('emailAttribute', 'mail', 'ldap'), 
+('usernameAttribute', 'uid', 'ldap'),
+('idAttribute', 'uid', 'ldap'),
+('refreshUsers', '1', 'ldap'),
 ('auth.anonymousRegistration', '0', 'user'),
 ('auth.allowGuestAccess', '0', 'user'),
 ('auth.internalUsersCanInvite', '0', 'user');
 
 UPDATE `user` SET `auth_mode` = 'ldap' WHERE `user`.`username` = '__ADMIN__';
+
+COMMIT;
