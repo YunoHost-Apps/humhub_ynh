2bcb281b31e005cd4cc3a9803e020d972ac7cd06
Nils VAN ZUIJLEN <nils.van-zuijlen@mailo.com>
2021-05-25 15:53:10 +0200

    Define minimum username length to 1 char
    To allow for all LDAP users
    
    Fixes #20
    
    Signed-off-by: Nils VAN ZUIJLEN <nils.van-zuijlen@mailo.com>

--- conf/common.php
+++ conf/common.php
@@ -2,10 +2,15 @@
 
 return [
     'components' => [
-		'db' => [
+        'db' => [
             'dsn' => 'mysql:host=localhost;dbname=__DB_NAME__',
             'username' => '__DB_USER__',
             'password' => '__DB_PWD__',
         ],
+    ],
+    'modules' => [
+        'user' => [
+            'minimumUsernameLength' => 1
+        ]
     ]
 ];
