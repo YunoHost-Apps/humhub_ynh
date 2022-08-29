29fbe71ad76fa6341de58727482f3a845fbace44
yalh76 <yalh@yahoo.com>
2022-08-30 00:36:35 +0200

    Apply last example_ynh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,12 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.4"
+YNH_PHP_VERSION=7.4
 
-pkg_dependencies="php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-bz2 php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-apcu-bc php${YNH_PHP_VERSION}-apcu php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-ldap"
+php_dependencies="php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-bz2 php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-apcu-bc php${YNH_PHP_VERSION}-apcu php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-ldap"
+
+# dependencies used by the app (must be on a single line)
+pkg_dependencies="$php_dependencies"
 
 HUMHUB_AUTH_BASIC_VERSION=0.1.0
 HUMHUB_AUTH_BASIC_PATH="/protected/modules/auth-basic"
